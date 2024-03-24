1dc127f0424fc46ad6e2b474cb48f44be7a97295
Swasti3719 <swastiprakashbhanja3719@gmail.com>
2024-03-24 13:16:41 +0530

    err

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -6,15 +6,29 @@ const client = new PrismaClient() ;
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
-    // should add zod validation here
-    const user = await client.user.create({
-        data: {
-            username: body.username,
-            password: body.password
-        }
-    });
+    console.log(body.username) ;
 
-    console.log(user.id);
+    // should add zod validation here
+    try{
+        const user = await client.user.create({
+            data: {
+                username: body.username,
+                password: body.password
+            }
+        });    
+        console.log(user.id);
+    }
+    catch(err){
+        console.log(err) ;
+    }
+    
 
     return NextResponse.json({ message: "Signed up" });
+}
+
+export async function GET(){
+    return NextResponse.json({
+        name : "SwastiDon" ,
+        email : "swasti@gmail.com"
+    })
 }
